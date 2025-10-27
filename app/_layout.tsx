a9c934ed56b25e113b9501ee24163e10425ee66e
vck2004 <52705258+vck2004@users.noreply.github.com>
2025-10-27 07:52:46 +0700

    final

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,11 @@
-import { Stack } from 'expo-router';
 import '../global.css';
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: 'Home' }} />
-      <Stack.Screen name="onboarding/index" options={{ headerShown: false }} />
-    </Stack>
-  );
+  // return (
+  // <Stack>
+  //   <Stack.Screen name="index" options={{ title: 'Home' }} />
+  //   <Stack.Screen name="(example-code)/nav" options={{ title: 'Nav' }} />
+  //   <Stack.Screen name="onboarding/index" options={{ headerShown: false }} />
+  // </Stack>
+  // );
 }
--- app/test.tsx
+++
@@ -1,5 +0,0 @@
-import OnboardingScreen from '@/components/builder/OnboardingScreen';
-
-export default function Index1() {
-  return <OnboardingScreen />;
-}
