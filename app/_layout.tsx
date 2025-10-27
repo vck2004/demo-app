120f9dcedc8cbc0d9e6eecdf2ff3b3195daa9acf
vck2004 <52705258+vck2004@users.noreply.github.com>
2025-10-27 21:39:58 +0700

    v1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,13 @@
+import { Slot } from 'expo-router';
 import '../global.css';
 
 export default function RootLayout() {
-  // return (
-  // <Stack>
-  //   <Stack.Screen name="index" options={{ title: 'Home' }} />
-  //   <Stack.Screen name="(example-code)/nav" options={{ title: 'Nav' }} />
-  //   <Stack.Screen name="onboarding/index" options={{ headerShown: false }} />
-  // </Stack>
-  // );
+  return (
+    <Slot></Slot>
+    // <Stack>
+    //   <Stack.Screen name="index" options={{ title: 'Home' }} />
+    //   <Stack.Screen name="(example-code)/nav" options={{ title: 'Nav' }} />
+    //   <Stack.Screen name="onboarding/index" options={{ headerShown: false }} />
+    // </Stack>
+  );
 }
