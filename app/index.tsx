cb173d38bf4a373dc92da24125cfcefec31b1678
vck2004 <52705258+vck2004@users.noreply.github.com>
2025-10-28 07:53:32 +0700

    f

--- app/index.tsx
+++ app/index.tsx
@@ -4,56 +4,56 @@
 // import { useEffect } from 'react';
 // import { Text, View } from 'react-native';
 
-export default function HomeScreen() {
-  // useEffect(() => {
-  //   checkFirstTimeOpen();
-  // }, []);
-  // const checkFirstTimeOpen = async () => {
-  //   let onboarded = await getData('onboarded');
-  //   if (onboarded !== '1') {
-  //     router.replace('/onboarding');
-  //   }
-  // };
-  // return (
-  //   <View className="flex-1 justify-center items-center">
-  //     <Text className="m-6 uppercase font-bold text-3xl text-center">This is the homepage</Text>
-  //     <Button title="View Onboarding Again" onPress={() => router.replace('/onboarding')} />
-  //     <Button title="View Tab layout" onPress={() => router.push('/nav/login')} />
-  //     <Button title="Clear Storage" variant="danger" onPress={() => removeData('onboarded')} />
-  //   </View>
-  // );
-  // export default function OnboardingScreen() {
-  // const backToHome = () => {
-  //   storeData('onboarded', '1');
-  //   router.replace('/');
-  // };
-  // return (
-  //   <View className="flex-1 justify-center items-center">
-  //     <Onboarding
-  //       onDone={backToHome}
-  //       onSkip={backToHome}
-  //       pages={[
-  //         {
-  //           backgroundColor: '#000',
-  //           image: <Image source={require('../../assets/images/react-logo.png')} />,
-  //           title: 'Onboarding',
-  //           subtitle: 'Welcome',
-  //         },
-  //         {
-  //           backgroundColor: '#000',
-  //           image: <Image source={require('../../assets/images/react-logo.png')} />,
-  //           title: 'Onboarding',
-  //           subtitle: 'This is the onboarding screen',
-  //         },
-  //         {
-  //           backgroundColor: '#000',
-  //           image: <Image source={require('../../assets/images/react-logo.png')} />,
-  //           title: 'Onboarding',
-  //           subtitle: 'Done with React Native Onboarding Swiper',
-  //         },
-  //       ]}
-  //     />
-  //   </View>
-  // );
-  // }
-}
+export default function HomeScreen() {}
+
+// useEffect(() => {
+//   checkFirstTimeOpen();
+// }, []);
+// const checkFirstTimeOpen = async () => {
+//   let onboarded = await getData('onboarded');
+//   if (onboarded !== '1') {
+//     router.replace('/onboarding');
+//   }
+// };
+// return (
+//   <View className="flex-1 justify-center items-center">
+//     <Text className="m-6 uppercase font-bold text-3xl text-center">This is the homepage</Text>
+//     <Button title="View Onboarding Again" onPress={() => router.replace('/onboarding')} />
+//     <Button title="View Tab layout" onPress={() => router.push('/nav/login')} />
+//     <Button title="Clear Storage" variant="danger" onPress={() => removeData('onboarded')} />
+//   </View>
+// );
+// export default function OnboardingScreen() {
+// const backToHome = () => {
+//   storeData('onboarded', '1');
+//   router.replace('/');
+// };
+// return (
+//   <View className="flex-1 justify-center items-center">
+//     <Onboarding
+//       onDone={backToHome}
+//       onSkip={backToHome}
+//       pages={[
+//         {
+//           backgroundColor: '#000',
+//           image: <Image source={require('../../assets/images/react-logo.png')} />,
+//           title: 'Onboarding',
+//           subtitle: 'Welcome',
+//         },
+//         {
+//           backgroundColor: '#000',
+//           image: <Image source={require('../../assets/images/react-logo.png')} />,
+//           title: 'Onboarding',
+//           subtitle: 'This is the onboarding screen',
+//         },
+//         {
+//           backgroundColor: '#000',
+//           image: <Image source={require('../../assets/images/react-logo.png')} />,
+//           title: 'Onboarding',
+//           subtitle: 'Done with React Native Onboarding Swiper',
+//         },
+//       ]}
+//     />
+//   </View>
+// );
+// }
